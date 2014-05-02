749d6d8f738c896eba95a36c937e617d85b92e8e
tmdevries <tmdevries@cox.net>
2014-05-01 21:51:47 -0700

    Added in comments
    Improves code readability for submission of phase 3.

--- PerfectFit/app/controllers/users_controller.rb
+++ PerfectFit/app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-	@@SWE = false
 	
 	before_filter :authorize, only: [ :change_email, :set_profile, :dashboard, :update, :change_password, :distroy, :set_admin]
 
@@ -11,6 +10,9 @@ class UsersController < ApplicationController
 	    redirect_to users_url
   	end
 
+  	# responsible for the functionality of granting or removing admin priviledges 
+  	# to other users. The logged in user must already be an admin for this functionality
+  	# to be offered to them. If the user is not admin, they are redirected to their dashboard
   	def set_admin
   		if(current_user.admin)
 			@user = User.find(params[:id]) #.update(params[:profile.first_name => "ADMIN HAHA"])
@@ -36,24 +38,22 @@ class UsersController < ApplicationController
 	end
 
 	def new
-	  @@SWE = true
 	  @user = User.new
 	  @user.updating_password = true
 	  @user.updating_email = true
 
-	  @@SWE = true
 	end
 
+	# Called when a new user is being created. If it is the first user of the site,
+	# they are given admin priviledges.
 	def create
 	  @user = User.new(params[:user])
-	  @@SWE = true
 	  @user.updating_password = true
 	  @user.updating_email = true
 	
 	  
 	  if @user.save
 	    cookies.permanent[:auth_token] = @user.auth_token
-	    @@SWE = true
 	      if (@user.id = 1)
 	  	@user.update_attributes(admin: true)
 	  end
@@ -67,17 +67,13 @@ class UsersController < ApplicationController
 	end
 
 	def set_profile
-		@@SWE = true
 		@user = current_user
-		@@SWE = true
 		@profile = @user.build_profile
-		@@SWE = true
 		
 		
 	end
 
 	def report
-		@@SWE = false
 		@user = current_user
 		user = current_user
 		@activities = @user.activities
@@ -88,9 +84,10 @@ class UsersController < ApplicationController
 		else
 			user.display_time = 1.weeks.ago
 		end
-		@@SWE = false
 	end
 
+	# responsible for when the admin panel page is accessed, verifying that the 
+	# logged in user is an admin. Otherwise, it redirects the user to their dashboard
 	def index
     	@users = User.all
     	if(current_user.admin)
@@ -106,13 +103,10 @@ class UsersController < ApplicationController
 
 
 	def change_email_password
-		@@SWE = false
 		@user = current_user
-		@@SWE = false
 	end
 
 	def dashboard
-		@@SWE = false
 		@user = current_user
 		user = current_user
 		@activities = @user.activities
@@ -123,9 +117,10 @@ class UsersController < ApplicationController
 		else
 			user.display_time = 1.weeks.ago
 		end
-		@@SWE = false
 	end
 
+	# When a user is logged in, trying to access the homepage of the website redirects
+	# them to their dashboard
 	def home_page
 		if(current_user)
 			redirect_to "/dashboard"
@@ -134,6 +129,13 @@ class UsersController < ApplicationController
 		end
 	end
 
+	# When a user is created, we want them to receive a welcome email to the email 
+	# address they used to sign up. This is also the same function executed when a 
+	# profile is updated. To differentiate the results, it is checked whether any
+	# activities have been entered (which means you have not used the application 
+	# before)
+	# In retrospect there is a more sophisticated way to ensure that users are not 
+	# sent welcome emails more than once.
 	def update
 		
 		@user = current_user
